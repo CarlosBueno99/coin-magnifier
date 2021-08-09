8f087d3f0cfc3a0b8bed4d19af351fd5f98088f7
CarlosBueno99 <carlosbueno.contato@gmail.com>
2021-08-09 18:22:03 -0300

    simple thing

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,28 @@
+import {useState} from 'react';
+
 function Home(){
-    return <div>simply a place holder thingy
-    </div>
+    return(
+        <div>
+            <h1>Eaí fiote!!</h1>
+            <Contador />
+        </div>
+    )
 }
+
+function Contador(){
+    const [contador, setContador] = useState(0);
+
+    function adcionarContador(){
+        setContador(contador + 1);
+    }
+    return(
+        <div>
+            <div>{contador}</div>
+            <button onClick={adcionarContador}>Adicionar Contador</button>
+        </div>
+    )
+}
+
+
 
 export default Home
